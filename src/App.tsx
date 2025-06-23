2e521d82065aca2b7fc1d2b1eb00d04e697d3c5d
Elroy Quan <elroyquan@gmail.com>
2025-06-23 00:29:53 -0400

    add files

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Message from './Message' //import message function
+
+
+
+function App(){
+
+  //note that <Message /> is the same thing as <Message> <Message> (just closes the tags)\
+  const name = 'Mosh';
+  return <div> <Message /> </div>;
+
+}
+
+export default App;
+
---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+//create a react component
+
+//Pascal Casing (capitalize first letter of every word)
+function Message(){
+    //JSX (javascript XML)
+    return <h1>hello world</h1>;
+
+}
+
+//make this function global.
+export default Message;
